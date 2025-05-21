f3924218d7a64f3ad211944ae2dba174d456dcd9
HusseinElbhrawy <hussein.elbhrawy74@gmail.com>
2025-05-21 20:27:05 +0300

    Refactor VolumeInfo class and update featured books list view to use state management
    - call fetchFeaturedBooks from bloc

--- lib/features/home/presentation/views/widgets/featured_books_listview.dart
+++ lib/features/home/presentation/views/widgets/featured_books_listview.dart
@@ -1,13 +1,10 @@
-import 'package:book_app/features/home/domain/entities/book_entity.dart';
 import 'package:book_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:book_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key, required this.books});
-
-  final List<BookEntity> books;
+  const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +15,14 @@ class FeaturedBooksListView extends StatelessWidget {
             height: MediaQuery.of(context).size.height * 0.25,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: 5,
+              itemCount: state.books.length,
               shrinkWrap: true,
               padding: const EdgeInsets.symmetric(horizontal: 20),
               physics: const BouncingScrollPhysics(),
               itemBuilder: (context, index) {
                 return Padding(
                   padding: const EdgeInsets.only(left: 10),
-                  child: CustomBookImage(image: books[index].image.toString()),
+                  child: CustomBookImage(image: state.books[index].image.toString()),
                 );
               },
             ),
